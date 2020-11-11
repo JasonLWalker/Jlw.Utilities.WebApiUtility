d4f170de0305c38a619ae60df54937539752c5dc
Jason Walker <jlwalker@spsmail.org>
2020-11-11 10:56:54 -0600

    hotfix for serializing

--- Jlw.Standard.Utilities.WebApiUtility/Models/ApiMessageType.cs
+++ Jlw.Standard.Utilities.WebApiUtility/Models/ApiMessageType.cs
@@ -8,21 +8,14 @@ namespace Jlw.Standard.Utilities.WebApiUtility
     /// <summary>
     /// Enumeration of Message types. With the exception of the Redirect, these correspond to the Bootstrap 3 and Toastr message class types.
     /// </summary>
-    [DataContract]
-    [JsonConverter(typeof(StringEnumConverter))]
+    [JsonConverter(typeof(JsonConverter<int>))]
     public enum ApiMessageType
     {
-        [EnumMember(Value = "0")]
-        Success,
-        [EnumMember(Value = "1")]
-        Warning,
-        [EnumMember(Value = "2")]
-        Info,
-        [EnumMember(Value = "3")]
-        Danger,
-        [EnumMember(Value = "4")]
-        Alert,
-        [EnumMember(Value = "5")]
-        Redirect
+        Success=0,
+        Warning=1,
+        Info=2,
+        Danger=3,
+        Alert=4,
+        Redirect=5
     }
 }
